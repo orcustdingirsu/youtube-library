c02c826a9db74ac361c6bc1c36b18daa97f2c7b3
orcustdingirsu <157905790+orcustdingirsu@users.noreply.github.com>
2024-01-27 19:56:31 +0100

    Update main.py

--- playlist/main.py
+++ playlist/main.py
@@ -492,12 +492,12 @@ def loginuser():
             if user == username and pwd == password:
                 # redirect to set user data url and send data to authenticate
                 id = rand_id()
-                redirect(request.url_root + '../setuserdata?username=' + username + '&password=' + password + '&id=' + id)
+                return redirect(request.url_root + '../setuserdata?username=' + username + '&password=' + password + '&id=' + id)
         # catch error
-        redirect('../login')
+        return redirect('../login')
     else:
         # catch error
-        redirect('../login')
+        return redirect('../login')
     
 
 if __name__ == '__main__':
